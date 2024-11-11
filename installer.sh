bd9a376732a003d1a69b57019ed8b2fe2dae5f99
TwoBitDev/OXC7R <0xC7R@proton.me>
2024-11-11 17:12:09 +0000

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Updating your system using apt && apt-get then installing the following:\npip\npython3\nproxychains\ntornet via pip"
+sudo apt update && sudo apt-get update && sudo apt install pip, python3, proxychains && pip install tornet
