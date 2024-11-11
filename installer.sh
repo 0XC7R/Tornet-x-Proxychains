4666243e4e74872d1a93caf5156950f6998a32db
TwoBitDev/OXC7R <0xC7R@proton.me>
2024-11-11 21:02:11 +0000

    Update installer.sh
    issue with installing forgot to check rofl

--- installer.sh
+++ installer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Updating your system using apt && apt-get then installing the following:\npip\npython3\nproxychains\ntornet via pip"
-sudo apt update && sudo apt-get update && sudo apt install pip, python3, proxychains && pip install tornet
+sudo apt update && sudo apt-get update && sudo apt install pip && sudo apt install python3 && sudo apt install proxychains && pip install tornet
