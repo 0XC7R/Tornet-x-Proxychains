405ea842b09979335f59aac570df008d514386ff
TwoBitDev/OXC7R <0xC7R@proton.me>
2024-11-12 16:29:23 +0000

    Update installer.sh
    added install for proxychains4 & libproxychains4

--- installer.sh
+++ installer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "Updating your system using apt && apt-get then installing the following:\npip\npython3\nproxychains\ntornet via pip"
-sudo apt update && sudo apt-get update && sudo apt install pip && sudo apt install python3 && sudo apt install proxychains && pip install tornet
+echo "Updating your system using apt && apt-get then installing the following:\npip\npython3\nproxychains4 & libproxychains4\ntornet via pip"
+sudo apt update && sudo apt-get update && sudo apt install pip && sudo apt install python3 && sudo apt install proxychains && sudo apt install proxychains4 && sudo apt install libproxychains4 && pip install tornet
